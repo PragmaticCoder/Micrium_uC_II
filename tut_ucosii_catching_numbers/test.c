4bb80fec81d6ccd7e6a48012ad5bde0e0e0c2c3f
Alvi Akbar <alvi.akbar@outlook.com>
2020-02-22 03:49:33 -0600

    created a skeleton task elapsed time and set it up to run periodically after 1 second

--- tut_ucosii_catching_numbers/test.c
+++ tut_ucosii_catching_numbers/test.c
@@ -20,11 +20,12 @@
 *********************************************************************************************************
 */
 
-#define  TASK_STK_SIZE		512		// Size of each task's stacks (# of WORDs)
+#define TASK_STK_SIZE 512 // Size of each task's stacks (# of WORDs)
 
-#define	TaskStartPrio		 1
-#define	TaskScanKeyPrio		10
-#define	TaskDisplayPrio		 9
+#define TaskStartPrio 1
+#define TaskScanKeyPrio 10
+#define TaskDisplayPrio 9
+#define TaskElapsedPrio 2
 
 /*
 *********************************************************************************************************
@@ -32,9 +33,10 @@
 *********************************************************************************************************
 */
 
-OS_STK  TaskStartStk[TASK_STK_SIZE];
-OS_STK	TaskScanKeyStk[TASK_STK_SIZE];
-OS_STK	TaskDisplayStk[TASK_STK_SIZE];
+OS_STK TaskStartStk[TASK_STK_SIZE];
+OS_STK TaskScanKeyStk[TASK_STK_SIZE];
+OS_STK TaskDisplayStk[TASK_STK_SIZE];
+OS_STK TaskElapsedTimeStk[TASK_STK_SIZE];
 
 // Declare semaphores here
 
@@ -44,12 +46,12 @@ OS_STK	TaskDisplayStk[TASK_STK_SIZE];
 *********************************************************************************************************
 */
 
-        void  TaskStart(void *data);                  /* Function prototypes of Startup task           */
-		void  TaskScanKey(void *data);
-		void  TaskDisplay(void *data);
-static  void  TaskStartCreateTasks(void);
-static  void  TaskStartDispInit(void);
-
+void TaskStart(void *data); /* Function prototypes of Startup task           */
+void TaskScanKey(void *data);
+void TaskDisplay(void *data);
+void TaskElapsedTime(void *data);
+static void TaskStartCreateTasks(void);
+static void TaskStartDispInit(void);
 
 /*$PAGE*/
 /*
@@ -58,40 +60,40 @@ static  void  TaskStartDispInit(void);
 *********************************************************************************************************
 */
 
-int  main (void)
+int main(void)
 {
-    PC_DispClrScr(DISP_FGND_WHITE + DISP_BGND_BLACK);	// Clear the screen
+    PC_DispClrScr(DISP_FGND_WHITE + DISP_BGND_BLACK); // Clear the screen
 
-    OSInit();											// Initialize uC/OS-II   
+    OSInit(); // Initialize uC/OS-II
 
     // Create semaphores here
-	
+
     OSTaskCreate(TaskStart, (void *)0, &TaskStartStk[TASK_STK_SIZE - 1], TaskStartPrio);
 
-    OSStart();											// Start multitasking
+    OSStart(); // Start multitasking
     return 0;
 }
-
 
 /*
 *********************************************************************************************************
 *                                              STARTUP TASK
 *********************************************************************************************************
 */
-void  TaskStart (void *pdata)
+void TaskStart(void *pdata)
 {
 
-	pdata = pdata;					// Prevent compiler warning
+    pdata = pdata; // Prevent compiler warning
 
-    TaskStartDispInit();			// Initialize the display
+    TaskStartDispInit(); // Initialize the display
 
-    TaskStartCreateTasks();			// Create all the application tasks
+    TaskStartCreateTasks(); // Create all the application tasks
 
     //OSStatInit();					// Initialize uC/OS-II's statistics
-	
-	for (;;) {
-		OSTaskSuspend(TaskStartPrio);
-	}
+
+    for (;;)
+    {
+        OSTaskSuspend(TaskStartPrio);
+    }
 }
 
 /*
@@ -100,64 +102,63 @@ void  TaskStart (void *pdata)
 *********************************************************************************************************
 */
 
-static  void  TaskStartDispInit (void)		// Do NOT make any change to TaskStartDispInit()
+static void TaskStartDispInit(void) // Do NOT make any change to TaskStartDispInit()
 {
-/*                                1111111111222222222233333333334444444444555555555566666666667777777777 */
-/*                      01234567890123456789012345678901234567890123456789012345678901234567890123456789 */
+    /*                                1111111111222222222233333333334444444444555555555566666666667777777777 */
+    /*                      01234567890123456789012345678901234567890123456789012345678901234567890123456789 */
     //PC_DispStr( 0,  0, "                                                                                ", DISP_FGND_WHITE + DISP_BGND_RED);
     //PC_DispStr( 0,  1, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
-    PC_DispStr( 0,  0, "                                                                                ", DISP_FGND_WHITE + DISP_BGND_WHITE);
-    PC_DispStr( 0,  1, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0,  2, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0,  3, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0,  4, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0,  5, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0,  6, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0,  7, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0,  8, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0,  9, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0, 10, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0, 11, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0, 12, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0, 13, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0, 14, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0, 15, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0, 16, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0, 17, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0, 18, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0, 19, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0, 20, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0, 21, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0, 22, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-    PC_DispStr( 0, 23, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
-/*                                1111111111222222222233333333334444444444555555555566666666667777777777 */
-/*                      01234567890123456789012345678901234567890123456789012345678901234567890123456789 */
-	PC_DispStr( 70,  0, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70,  1, " CATCH    ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70,  2, " THE      ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70,  3, " NUMBERS! ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70,  4, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70,  5, " SCORE:   ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70,  6, " 00000000 ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70,  7, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70,  8, " TIME:    ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70,  9, " 00:00:00 ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70, 10, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70, 11, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70, 12, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70, 13, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70, 14, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70, 15, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70, 16, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70, 17, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70, 18, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70, 19, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70, 20, " PRESS    ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70, 21, " 'ESC'    ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70, 22, " TO QUIT  ", DISP_FGND_WHITE + DISP_BGND_RED);
-	PC_DispStr( 70, 23, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(0, 0, "                                                                                ", DISP_FGND_WHITE + DISP_BGND_WHITE);
+    PC_DispStr(0, 1, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 2, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 3, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 4, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 5, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 6, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 7, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 8, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 9, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 10, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 11, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 12, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 13, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 14, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 15, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 16, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 17, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 18, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 19, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 20, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 21, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 22, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    PC_DispStr(0, 23, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_WHITE);
+    /*                                1111111111222222222233333333334444444444555555555566666666667777777777 */
+    /*                      01234567890123456789012345678901234567890123456789012345678901234567890123456789 */
+    PC_DispStr(70, 0, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 1, " CATCH    ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 2, " THE      ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 3, " NUMBERS! ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 4, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 5, " SCORE:   ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 6, " 00000000 ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 7, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 8, " TIME:    ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 9, " 00:00:00 ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 10, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 11, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 12, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 13, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 14, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 15, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 16, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 17, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 18, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 19, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 20, " PRESS    ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 21, " 'ESC'    ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 22, " TO QUIT  ", DISP_FGND_WHITE + DISP_BGND_RED);
+    PC_DispStr(70, 23, "          ", DISP_FGND_WHITE + DISP_BGND_RED);
 }
-
 
 /*
 *********************************************************************************************************
@@ -165,12 +166,13 @@ static  void  TaskStartDispInit (void)		// Do NOT make any change to TaskStartDi
 *********************************************************************************************************
 */
 
-static  void  TaskStartCreateTasks (void)
+static void TaskStartCreateTasks(void)
 {
     //INT8U  i;
 
-	OSTaskCreate(TaskScanKey, (void *) 0, &TaskScanKeyStk[TASK_STK_SIZE - 1], TaskScanKeyPrio);
-	OSTaskCreate(TaskDisplay, (void *) 0, &TaskDisplayStk[TASK_STK_SIZE - 1], TaskDisplayPrio);
+    OSTaskCreate(TaskScanKey, (void *)0, &TaskScanKeyStk[TASK_STK_SIZE - 1], TaskScanKeyPrio);
+    OSTaskCreate(TaskDisplay, (void *)0, &TaskDisplayStk[TASK_STK_SIZE - 1], TaskDisplayPrio);
+    OSTaskCreate(TaskElapsedTime, (void *)0, &TaskElapsedTimeStk[TASK_STK_SIZE - 1], TaskElapsedPrio);
 }
 
 /*
@@ -179,35 +181,47 @@ static  void  TaskStartCreateTasks (void)
 *********************************************************************************************************
 */
 
-void  TaskDisplay (void *pdata)
+void TaskDisplay(void *pdata)
 {
-	//INT8U err;
-	char s[15];			// larger string for hh:mm:ss
-	
-    for (;;) {
-		OSTimeDlyHMSM(0, 0, 1, 0);
-		//sprintf(s, "%02d:%02d:%02d", elapsed_time_hh, elapsed_time_mm, elapsed_time_ss); 
-		//PC_DispStr( 71,  9, s, DISP_FGND_WHITE + DISP_BGND_RED);
-		//sprintf(s, "%8u", total_score);
-		//PC_DispStr(71, 6, s, DISP_FGND_WHITE + DISP_BGND_RED);
-	}
+    //INT8U err;
+    char s[15]; // larger string for hh:mm:ss
+
+    for (;;)
+    {
+        OSTimeDlyHMSM(0, 0, 1, 0);
+        //sprintf(s, "%02d:%02d:%02d", elapsed_time_hh, elapsed_time_mm, elapsed_time_ss);
+        //PC_DispStr( 71,  9, s, DISP_FGND_WHITE + DISP_BGND_RED);
+        //sprintf(s, "%8u", total_score);
+        //PC_DispStr(71, 6, s, DISP_FGND_WHITE + DISP_BGND_RED);
+    }
 }
 
-void  TaskScanKey (void *pdata)
+void TaskScanKey(void *pdata)
 {
-	INT16S     key;
+    INT16S key;
 
-    for (;;) {
-        if (PC_GetKey(&key) == TRUE) {	// See if key has been pressed
-            switch (key) {
-            case (0x1B):				// See if it's the ESCAPE key
-                exit(0);				// End program
-            case ('D'):					// See if it's the D key.
+    for (;;)
+    {
+        if (PC_GetKey(&key) == TRUE)
+        { // See if key has been pressed
+            switch (key)
+            {
+            case (0x1B): // See if it's the ESCAPE key
+                exit(0); // End program
+            case ('D'):  // See if it's the D key.
             case ('d'):
-				break;
-			}
-		}
+                break;
+            }
+        }
         OSTimeDly(1);
     }
 }
 
+void TaskElapsedTime(void *pdata)
+{
+    for (;;)
+    {
+        log_info("%4u: ***** TaskElapsedTime *****\n", OSTime);
+        OSTimeDly(1);
+    }
+}
