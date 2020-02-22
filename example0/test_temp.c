0d0432864d99457f0f68ea82f9c214c845f78b7a
Alvi Akbar <alvi.akbar@outlook.com>
2020-02-22 01:50:15 -0600

    minor formatting

--- example0/test_temp.c
+++ example0/test_temp.c
@@ -20,31 +20,31 @@
 *********************************************************************************************************
 */
 
-#define TASK_STK_SIZE   1024                                    // Stack size, in bytes
+#define TASK_STK_SIZE 1024 // Stack size, in bytes
 
 //#define SUSPEND_RESUME                                        // Task 1 and 2 use suspend and resume for scheduling
-                                                                // (if not defined: use time delays)
+// (if not defined: use time delays)
 
 /*
 *********************************************************************************************************
 *                                               VARIABLES
 *********************************************************************************************************
 */
-OS_STK  TestTaskStk1[TASK_STK_SIZE];                            // Task stacks
-OS_STK  TestTaskStk2[TASK_STK_SIZE];
-OS_STK  TestTaskStk3[TASK_STK_SIZE];
-OS_STK  TestTaskStk4[TASK_STK_SIZE];
+OS_STK TestTaskStk1[TASK_STK_SIZE]; // Task stacks
+OS_STK TestTaskStk2[TASK_STK_SIZE];
+OS_STK TestTaskStk3[TASK_STK_SIZE];
+OS_STK TestTaskStk4[TASK_STK_SIZE];
 
 /*
 *********************************************************************************************************
 *                                           FUNCTION PROTOTYPES
 *********************************************************************************************************
 */
-void TestTask1(void *pdata);                                    // The 3 tasks
+void TestTask1(void *pdata); // The 3 tasks
 void TestTask2(void *pdata);
 void TestTask3(void *pdata);
 void TestTask4(void *pdata);
-void OurIsr1(void);                                             // The interrupt service routine for IRQ 1
+void OurIsr1(void); // The interrupt service routine for IRQ 1
 
 /*
 *********************************************************************************************************
@@ -55,22 +55,22 @@ int main(void)
 {
     // Display a banner.
 #ifdef __WIN32__
-    printf("##### uCOS-II V%4.2f Port V%4.2f for WIN32 #####\n", ((FP32)OSVersion())/100, ((FP32)OSPortVersion())/100);
+    printf("##### uCOS-II V%4.2f Port V%4.2f for WIN32 #####\n", ((FP32)OSVersion()) / 100, ((FP32)OSPortVersion()) / 100);
 #endif
 #ifdef __LINUX__
-    printf("##### uCOS-II V%4.2f Port V%4.2f for LINUX #####\n", ((FP32)OSVersion())/100, ((FP32)OSPortVersion())/100);
+    printf("##### uCOS-II V%4.2f Port V%4.2f for LINUX #####\n", ((FP32)OSVersion()) / 100, ((FP32)OSPortVersion()) / 100);
 #endif
 
-     // Initialize uCOS-II.
-    OSInit();                                                               //Calling sequence -->OSInitHookBegin-->OSTaskStkInit-->OSTCBInitHook-->OSTaskCreateHook-->OSInitHookEnd
+    // Initialize uCOS-II.
+    OSInit(); //Calling sequence -->OSInitHookBegin-->OSTaskStkInit-->OSTCBInitHook-->OSTaskCreateHook-->OSInitHookEnd
 
     // Create the first task
-    OSTaskCreate(TestTask1, (void *) 11, &TestTaskStk1[TASK_STK_SIZE], 11); //Calling sequence -->OSTaskStkInit-->OSTCBInitHook-->OSTaskCreateHook
+    OSTaskCreate(TestTask1, (void *)11, &TestTaskStk1[TASK_STK_SIZE], 11); //Calling sequence -->OSTaskStkInit-->OSTCBInitHook-->OSTaskCreateHook
 
-    PC_IntVectSet(1, OurIsr1);                                              //Install an interrupt service routine for IRQ 1
+    PC_IntVectSet(1, OurIsr1); //Install an interrupt service routine for IRQ 1
 
     // Start multitasking.
-    OSStart();                                                              //Calling sequence -->OSTaskSwHook-->OSStartHighRdy
+    OSStart(); //Calling sequence -->OSTaskSwHook-->OSStartHighRdy
 
     /* NEVER EXECUTED */
     printf("main(): We should never execute this line\n");
@@ -78,30 +78,31 @@ int main(void)
     return 0;
 }
 
-
 /*
 *********************************************************************************************************
 *                                                First Task (startup task)
 *********************************************************************************************************
 */
 void TestTask1(void *pdata)
-{   printf("%4u: ***** Test Task 1 First call *****\n", OSTime);
+{
+    printf("%4u: ***** Test Task 1 First call *****\n", OSTime);
 
 #if OS_TASK_STAT_EN > 0
-    OSStatInit();                                                               //Initialize the statistics task
+    OSStatInit(); //Initialize the statistics task
 #endif
 
-    OSTaskCreate(TestTask2, (void *) 22, &TestTaskStk2[TASK_STK_SIZE], 22);     //Create 3 other tasks
-    OSTaskCreate(TestTask3, (void *) 33, &TestTaskStk3[TASK_STK_SIZE], 33);
-    OSTaskCreate(TestTask4, (void *) 10, &TestTaskStk4[TASK_STK_SIZE], 10);
+    OSTaskCreate(TestTask2, (void *)22, &TestTaskStk2[TASK_STK_SIZE], 22); //Create 3 other tasks
+    OSTaskCreate(TestTask3, (void *)33, &TestTaskStk3[TASK_STK_SIZE], 33);
+    OSTaskCreate(TestTask4, (void *)10, &TestTaskStk4[TASK_STK_SIZE], 10);
 
     while (1)
-    {   printf("%4u: ***** Test Task 11 *****\n", OSTime);
+    {
+        printf("%4u: ***** Test Task 11 *****\n", OSTime);
 
 #ifdef SUSPEND_RESUME
-        OSTaskSuspend(OS_PRIO_SELF);                                            //Calling sequence -->OSTaskSwHook-->OSCtxSw
+        OSTaskSuspend(OS_PRIO_SELF); //Calling sequence -->OSTaskSwHook-->OSCtxSw
 #else
-        OSTimeDly(1);                                                           //Calling sequence -->OSTaskSwHook-->OSCtxSw
+        OSTimeDly(1); //Calling sequence -->OSTaskSwHook-->OSCtxSw
 #endif
     }
 }
@@ -112,8 +113,10 @@ void TestTask1(void *pdata)
 *********************************************************************************************************
 */
 void TestTask2(void *pdata)
-{   while (1)
-    {   printf("%4u: ***** Test Task 22 *****\n", OSTime);
+{
+    while (1)
+    {
+        printf("%4u: ***** Test Task 22 *****\n", OSTime);
 
 #ifdef SUSPEND_RESUME
         OSTaskSuspend(OS_PRIO_SELF);
@@ -129,7 +132,8 @@ void TestTask2(void *pdata)
 *********************************************************************************************************
 */
 void TestTask3(void *pdata)
-{   while (1)
+{
+    while (1)
     {
 #ifdef SUSPEND_RESUME
         printf("%4u: ***** Test Task 33 *****\n", OSTime);
@@ -142,11 +146,12 @@ void TestTask3(void *pdata)
         OSTimeDly(1);
 
         if (_kbhit())
-	{   OSTaskDel(10);
-	    OSTaskDel(11);
-	    OSTaskDel(22);
-	    exit(0);
-	}
+        {
+            OSTaskDel(10);
+            OSTaskDel(11);
+            OSTaskDel(22);
+            exit(0);
+        }
     }
 }
 
@@ -156,13 +161,13 @@ void TestTask3(void *pdata)
 *********************************************************************************************************
 */
 void TestTask4(void *pdata)
-{   while (1)
+{
+    while (1)
     {
         printf("%4u: +++++ Test Task 40 +++++\n", OSTime);
-        OSTaskSuspend(10);                                      //Suspend yourself
+        OSTaskSuspend(10); //Suspend yourself
     }
 }
-
 
 /*
 *********************************************************************************************************
@@ -171,8 +176,9 @@ void TestTask4(void *pdata)
 *********************************************************************************************************
 */
 void OurIsr1(void)
-{       OSIntEnter();
-        printf("##### Interrupt service routine for IRQ 1 #####\n");
-        OSTaskResume(10);                                       //Trigger task 4
-        OSIntExit();
+{
+    OSIntEnter();
+    printf("##### Interrupt service routine for IRQ 1 #####\n");
+    OSTaskResume(10); //Trigger task 4
+    OSIntExit();
 }
